86bc661d856f0d44768e8cc9c9a10e80e58d2eab
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2020-07-25 11:33:17 +0900

    vuex sample

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>{{ count }}</p>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </p>
+    <input type="number" v-model="amount">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    data () {
+      return {
+        amount: 1
+      }
+    },
+    computed: {
+        count () {
+          return this.$store.state.count
+        }
+    },
+    methods: {
+      increment () {
+        this.$store.commit('increment', this.amount)
+      },
+      decrement () {
+        this.$store.commit('decrement', this.amount)
+      }
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment: (state, amount) => state.count += parseInt(amount, 10),
+    decrement: (state, amount) => state.count -= parseInt(amount, 10)
+  }
+})
+
 new Vue({
   render: h => h(App),
+  store,
 }).$mount('#app')
