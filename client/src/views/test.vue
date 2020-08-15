180d45a4f70474aebf0869ff47955d98047ca32b
ohayama4z <ohyama4z000@gmail.com>
2020-08-15 18:44:44 +0900

    test.vueのscriptのdata部分

--- client/src/views/test.vue
+++ client/src/views/test.vue
@@ -11,43 +11,7 @@ export default {
   name: 'test',
   data() {
     return {
-      isOpened: false,
-      header: [{
-        header: true,
-        title: '過去問管理',
-        hiddenOnCollapse: true
-      }],
-      menu: [{
-              title: '名前なし',
-              icon: this.isOpened ? 'fa fa-folder-open' : 'fa fa-folder',
-              child: [
-            {
-              title: 'file001',
-              icon: 'fa fa-file',
-            }
-          ]
-        },
-        {
-          title: '名前あり',
-          icon: this.isOpened ? 'fa fa-folder-open' : 'fa fa-folder',
-          child: [
-            {
-                title: '英語',
-                icon: this.isOpened ? 'fa fa-folder-open' : 'fa fa-folder',
-              child: [
-                {
-                  title: 'file002',
-                  icon: 'fa fa-file',
-                }
-              ]
-            },
-            {
-              title: '数学',
-              icon: this.isOpened ? 'fa fa-folder-open' : 'fa fa-folder',
-            }
-          ]
-        }
-      ]
+      state: null
     }
   },
   computed: {
