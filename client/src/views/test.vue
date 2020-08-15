d0bf91f27098972d63e9c6d5a6a226ab1549ed09
ohayama4z <ohyama4z000@gmail.com>
2020-08-15 18:46:35 +0900

    test.vueのcomputed部分

--- client/src/views/test.vue
+++ client/src/views/test.vue
@@ -15,68 +15,8 @@ export default {
     }
   },
   computed: {
-    sidebarMenu () {
-      return this.header.concat(this.getMenuStructure)
-    },
-    intermediateFiles () {
-      return this.$store.state.sampleFiles.reduce((previous, current) => {
-        if (previous == null) {
-          previous = {}
-        }
-
-        if (previous[current.year] == null) {
-          previous[current.year] = {}
-        }
-
-        if (previous[current.year][current.subject] == null) {
-          previous[current.year][current.subject] = {}
-        }
-
-        if (previous[current.year][current.subject][current.period] == null) {
-          previous[current.year][current.subject][current.period] = []
-        }
-
-        previous[current.year][current.subject][current.period].push(current)
-
-        return previous
-      }, {})
-    },
-    getMenuStructure () {
-      const icon = 'fa fa-folder'
-      return Object.entries(this.intermediateFiles).reduce((previous, [year, value]) => {
-        previous.push({
-          title: year,
-          icon,
-          child: generateChildOfYear(value)
-        })
-        return previous
-      }, [])
-
-      function generateChildOfYear(yearValue) {
-        return Object.entries(yearValue).reduce((previous, [subject, subjectValue]) => {
-          previous.push({
-            title: subject,
-            icon,
-            child: generateChildOfSubject(subjectValue)
-          })
-          return previous
-        }, [])
-      }
-
-      function generateChildOfSubject(subjectValue) {
-        return Object.entries(subjectValue).reduce((previous, [period, periodValue]) => {
-          previous.push({
-            title: period,
-            icon,
-            child: generateChildOfPeriod(periodValue)
-          })
-          return previous
-        }, [])
-      }
-
-      function generateChildOfPeriod(periodValue) {
-        return periodValue.map(file => file.fileName)
-      }
+    status () {
+      return this.$store.state.serverSideLanguage.status
     }
   },
 }
