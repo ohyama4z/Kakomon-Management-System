001a8662bd51ebdfd57b72b80c80814390c8d744
ohayama4z <ohyama4z000@gmail.com>
2020-08-16 11:54:17 +0900

    mock完全に理解した

--- client/src/views/__tests__/Login.test.js
+++ client/src/views/__tests__/Login.test.js
@@ -4,6 +4,8 @@ import Vuex from 'vuex'
 import VueRouter from 'vue-router'
 // import Mctions from '../../store/mutations'
 
+const netlifyIdentity = require('netlify-identity-widget')
+
 
 const localVue = createLocalVue()
 
@@ -11,6 +13,33 @@ localVue.use(Vuex)
 localVue.use(VueRouter)
 
 const router = new VueRouter()
+
+// netlifyIdentityの関数を使えるようにする
+
+// const netlifyIdentityMock = () => ({
+//     open: jest.fn(),
+//     on: jest.fn((event, callback) => {
+//         if(event === 'login') {
+//             callback()
+//         }
+//     })
+// })
+// const netlifyIdentityMock = jest.createMockFromModule('netlify-identity-widget')
+// netlifyIdentityMock.open = jest.fn()
+// netlifyIdentityMock.on = jest.fn((event, callback) => {
+//     if(event === 'login') {
+//         callback()
+//     }
+// })
+
+jest.mock('netlify-identity-widget')
+netlifyIdentity.open = jest.fn()
+netlifyIdentity.on = jest.fn().mockImplementation((event, callback) => {
+    if(event === 'login') {
+        callback()
+    }
+})
+
 
 describe('Login.vue', () => {
     it('currentUserがログイン済みのとき最後に開いたパスに飛ばすmutationが呼ばれ、lastPageに遷移する', () => {
@@ -30,8 +59,36 @@ describe('Login.vue', () => {
             router,
             store
         })
+
+        expect(netlifyIdentity.open).toHaveBeenCalled()
         expect(mutations.updateLastPage).toHaveBeenCalled()
         expect(wrapper.vm.$route.path).toBe(`/${store.state.lastPage}`)
+    })
+
+    it('netlify-identity-widgetでログインしたら最後に開いたパスに飛ばすmutationが呼ばれ、lastPageに遷移する', () => {
+        const mutations = {
+            getCurrentUser: jest.fn(),
+            updateLastPage: jest.fn()
+        }
+        const store = new Vuex.Store({
+            state: {
+                currentUser: true,
+                lastPage: 'edit'
+            },
+            mutations
+        })
+
+        const wrapper = shallowMount(Login, {
+            localVue,
+            router,
+            store
+        })
+
+        expect(netlifyIdentity.open).toHaveBeenCalled()
+        expect(netlifyIdentity.on).toHaveBeenCalled()
+        expect(mutations.updateLastPage).toHaveBeenCalled()
+        expect(wrapper.vm.$route.path).toBe(`/${store.state.lastPage}`)
+
     })
 })
 
