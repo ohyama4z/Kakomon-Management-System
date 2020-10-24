ad3bc95f853a8e52fd3da67be9d271e38a7d4f4f
asann3 <e.4610ke@gmail.com>
2020-10-24 13:11:03 +0000

    テストを書いた

--- client/src/store/__tests__/actions.test.js
+++ client/src/store/__tests__/actions.test.js
@@ -418,7 +418,7 @@ describe('actions.js', () => {
     expect(localStorage.setItem).not.toHaveBeenCalled()
   })
 
-  it('objectToCsvが機能するかテスト', () => {
+  it('convertObjToCsvが機能するかテスト', () => {
     const objarr = [
       {
         src: 'scanned/20180802_2年3紐。5組『倫理社会」前期定期試験1.jpg',
@@ -452,7 +452,7 @@ describe('actions.js', () => {
     expect(convertObjToCsv(objarr)).toEqual(result)
   })
 
-  it('setCommitCsv', async () => {
+  it('setCommitCsvが機能しているか', async () => {
     const state = JSON.parse(JSON.stringify(defaultState))
     state.currentBranch = 'cmstest'
     const branchName = state.currentBranch
@@ -796,7 +796,7 @@ describe('actions.js', () => {
             included_pages_num: '',
             period: '',
             src: 'tests/2018/テスト_2018_後期中間_論理回路i_問題001.jpg:',
-            subj: '算数',
+            subj: '論理回路',
             tool_type: '勉強用',
             year: ''
           },
@@ -811,6 +811,30 @@ describe('actions.js', () => {
             subj: '論理回路i',
             tool_type: 'テスト',
             year: '2018'
+          },
+          'scanned/20180802_2年3紐。5組『倫理社会」前期定期試験1.jpg': {
+            src: 'scanned/20180802_2年3紐。5組『倫理社会」前期定期試験1.jpg',
+            subj: '倫理社会',
+            tool_type: 'テスト',
+            period: '前期定期',
+            year: '2018',
+            content_type: '',
+            author: '',
+            image_index: '',
+            included_pages_num: '',
+            fix_text: ''
+          },
+          'scanned/20180802_2年3紐。5組『倫理社会」前期定期試験2.jpg': {
+            src: 'scanned/20180802_2年3紐。5組『倫理社会」前期定期試験2.jpg',
+            subj: '',
+            tool_type: '',
+            period: '',
+            year: '',
+            content_type: '',
+            author: '',
+            image_index: '',
+            included_pages_num: '',
+            fix_text: ''
           }
         }
       }
@@ -847,23 +871,24 @@ describe('actions.js', () => {
     // stateが変更されていないか
     expect(state.contentMetadatas[csvSha].data).toEqual(saveContentMetadatas)
 
-    // convertObjToCsvの動作確認
-    const convertedCsv =
+    // 編集後
+    const editedCsv =
       `\ufeff` +
       `src,subj,tool_type,period,year,content_type,author,image_index,included_pages_num,fix_text` +
-      `\n` +
-      `scanned/20180802_2年3紐。5組『倫理社会」前期定期試験1.jpg,倫理社会,テスト,前期定期,2018,,,,,` +
-      `\n` +
-      `scanned/20180802_2年3紐。5組『倫理社会」前期定期試験2.jpg,,,,,,,,,` +
       `\n` +
       `tests/2018/テスト_2018_後期中間_論理回路i_問題001.jpg:,算数,勉強用,,,,,,,` +
       `\n` +
       `tests/2018/テスト_2018_後期中間_論理回路i_問題002.jpg,論理回路i,テスト,後期中間,2018,問題,,002,1,` +
+      `\n` +
+      `scanned/20180802_2年3紐。5組『倫理社会」前期定期試験1.jpg,倫理社会,テスト,前期定期,2018,,,,,` +
+      `\n` +
+      `scanned/20180802_2年3紐。5組『倫理社会」前期定期試験2.jpg,,,,,,,,,` +
       `\n`
+    console.log(fetchMock.calls(undefined, 'POST')[0][1], 'contentsCheck')
     const persedConvertedBody = JSON.parse(
       fetchMock.calls(undefined, 'POST')[0][1].body
     )
-    expect(persedConvertedBody.content).toBe(convertedCsv)
+    expect(persedConvertedBody.content).toBe(editedCsv)
   })
 
   it('ファイル編集の際tokenがnullならエラー', async () => {
